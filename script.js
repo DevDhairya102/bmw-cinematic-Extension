c4d65b9a74caaa6e14f45bb65856d9f5ff031d53
Dhairya <dhairyamihir2005@gmail.com>
2026-06-16 10:00:54 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-
-function tick(){
-document.getElementById('clock').innerHTML=new Date().toLocaleTimeString([],{
-hour:'2-digit',minute:'2-digit',second:'2-digit'
-});
-}
-tick();
-setInterval(tick,1000);
